8a67a184acafb63580e4f6a21faf326115c80880
Daniel <dmagues@gmail.com>
2020-05-19 15:57:49 -0500

    about page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from "react-router-dom"
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Home extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          <Link className="App-link" to="/about">Link to the About Page</Link>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+export default Home;
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './App';
 import * as serviceWorker from './serviceWorker';
+/*HasRouter because we're note doing web request*/
+import { HashRouter as Router, Route } from 'react-router-dom';
+import About from './pages/About';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <div>
+      <main>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/about" component={About}></Route>
+      </main>
+    </div>
+  </Router>,  
   document.getElementById('root')
 );
 
